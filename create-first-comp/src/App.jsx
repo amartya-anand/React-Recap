439ffd377bba176663deb5ff1b6159cc3765d7df
Amartya Anand <dev.amartya@outlook.com>
2025-01-16 13:15:03 +0530

    Reused component

--- create-first-comp/src/App.jsx
+++ create-first-comp/src/App.jsx
@@ -1,11 +1,17 @@
 import LikeButton from "./LikeButton";
 import Hello from "./Hello";
+import Random from "./Random";
 function App() {
   return (
     <div>
       <h1>Cruising through components.</h1>
       <LikeButton></LikeButton>
       <Hello></Hello>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
     </div>
   );
 }
