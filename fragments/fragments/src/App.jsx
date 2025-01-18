b8c23d4ba72330a1bc0a1cce4790f64d74738ffa
Amartya Anand <dev.amartya@outlook.com>
2025-01-18 19:03:02 +0530

    First fragment created

---
+++ fragments/fragments/src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <h1>Healthy foods</h1>
+      <ul class="list-group">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
