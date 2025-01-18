357f177d2753b9d9301fb3c64485223f09eb843a
Amartya Anand <dev.amartya@outlook.com>
2025-01-18 19:40:27 +0530

    Conditional rendering

--- fragments/fragments/src/App.jsx
+++ fragments/fragments/src/App.jsx
@@ -2,9 +2,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   let foods = ["Apple", "Banana", "Orange", "Grapes", "Strawberry"];
+  //   let foods = [];
   return (
     <>
       <h1>Healthy foods</h1>
+      {foods.length === 0 ? <p>No food items available</p> : null}
       <ul className="food-items">
         {foods.map((item) => (
           <li key={item} className="list-group-item">
